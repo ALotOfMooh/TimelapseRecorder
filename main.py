a115fdf2578cc39ef4cfae621c8377ac83ada0c7
ALotOfMooh <foli@gmx.net>
2023-11-01 17:06:25 +0100

    update nach einer weile

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ img_file_prefix = "image_"
 img_index = 0
 img_shown_index = 0
 
+isTimelapse = False
+
 # Set up variable to store time when last picture was taken
 last_picture_time = time.time()
 last_pressed_time = time.time()
+program_start_time = time.time()
+
+# Create named window and make it resizable
+cv2.namedWindow("Live Stream", cv2.WINDOW_NORMAL)
 
 # Loop indefinitely
 while True:
@@ -30,6 +36,27 @@ while True:
             # Save frame as image file
             img_filename = "pictures/" + \
                 img_file_prefix + str(img_index) + ".jpg"
+            timestamp = int((current_time - last_picture_time) / 60)
+            # Add text to image
+            elapsed_time = int(current_time - program_start_time)
+            hours = elapsed_time // 3600
+            minutes = (elapsed_time % 3600) // 60
+            seconds = elapsed_time % 60
+            text = "Elapsed Time: {:02d}:{:02d}:{:02d}".format(
+                hours, minutes, seconds)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 0.5
+            font_thickness = 1
+            text_color = (255, 255, 255)
+            text_bg_color = (0, 0, 0)
+            (text_width, text_height) = cv2.getTextSize(
+                text, font, font_scale, font_thickness)[0]
+            img = cv2.rectangle(
+                frame, (0, 0), (text_width + 10, text_height + 10), text_bg_color, -1)
+            org = (5, text_height + 5)
+            img = cv2.putText(img, text, org, font, font_scale,
+                              text_color, font_thickness, cv2.LINE_AA)
+
             cv2.imwrite(img_filename, frame)
             last_picture_time = current_time
             # Display current image
@@ -43,14 +70,14 @@ while True:
     # Scroll back one picture if left arrow key is pressed
     if key == ord('a'):  # left arrow key
         img_shown_index -= 1
-        if img_shown_index < 0:
-            img_shown_index = 0
+        if img_shown_index < 1:
+            img_shown_index = 1
         img_filename = "pictures/" + img_file_prefix + \
             str(img_shown_index) + ".jpg"
         frame = cv2.imread(img_filename)
         cv2.imshow("Live Stream", frame)
         last_pressed_time = current_time
-    elif key == ord('s'):  # left arrow key
+    elif key == ord('s'):  # lefat arrow key
         img_shown_index += 1
         if img_shown_index > img_index:
             img_shown_index = img_index
